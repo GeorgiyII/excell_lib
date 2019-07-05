c9ea6024291a65e39eb72257e44cd59845982fe6
Heorhii <gosha1104@nxt.ru>
2019-07-05 15:36:35 +0300

    added simple ui, added requirements

--- table_executor.py
+++ table_executor.py
@@ -1,4 +1,5 @@
 import openpyxl
+import logging
 
 from data_master import get_materials_data, get_next_abbreviation_pack, get_merge_cells
 from excell_lib.actions import (
@@ -25,34 +26,46 @@ def add_column_with_prices(table, table_prices, symbol):
     return new_table
 
 
-def copy_worksheet(book):
-    if book.get_sheet_by_name('innervagg Copy'):
-        sheet = book.get_sheet_by_name('innervagg Copy')
+def copy_worksheet(book, sheet_name, new_sheet_name):
+    try:
+        sheet = book.get_sheet_by_name(new_sheet_name)
         book.remove(sheet)
-    sheet = book.get_sheet_by_name('innervagg')
-    book.copy_worksheet(sheet)
-    sheet = book.get_sheet_by_name('innervagg Copy')
+    except KeyError as err:
+        logging.info(str(err))
+        sheet = book.get_sheet_by_name(sheet_name)
+        book.copy_worksheet(sheet)
+        sheet = book.get_sheet_by_name(new_sheet_name)
+        logging.info('Create new sheet')
     return sheet
 
 
-def start(file_name):
-    rows = [1, 2, 3]
-    book = openpyxl.load_workbook(file_name)
-    sheet = copy_worksheet(book)
+def start(config):
+    logging.info('Start excel program')
+    rows = config.header_rows
+    book = openpyxl.load_workbook(config.file_path)
+    sheet = copy_worksheet(book, config.sheet_name, config.new_sheet_name)
     unmerge(sheet)
-    sheet_prices = book.get_sheet_by_name('model')
-    table = Table(sheet, 2)
+    sheet_prices = book.get_sheet_by_name(config.prices_sheet_name)
+    table = Table(sheet, config.row_with_params_number)
     for row in rows:
         constants_data = table.get_row_values(row)
         add_units(constants_data)
+    logging.info('Create main table')
     table_prices = Table(sheet=sheet_prices)
-    symbol = ';'
+    logging.info('Create prices table')
+    symbol = config.separator_symbol
+    if not symbol:
+        symbol = ';'
     new_table = add_column_with_prices(table, table_prices, symbol)
+    logging.info('New columns created')
     new_table.write_table(sheet)
+    logging.info('New tables wrote')
     merge_rows_numbers = [1, 2]
     for row_number in merge_rows_numbers:
         row_data = new_table.get_row_values(row_number)
         merge_cells = get_merge_cells(row_data, row_number)
         merge(sheet, merge_cells)
+    logging.info('Automerge complete')
 
-    book.save(file_name)
+    book.save(config.file_path)
+    logging.info('Excell save')
